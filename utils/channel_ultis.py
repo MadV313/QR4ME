fb7b20cb5bc833e398f73fa0c7136b69813f5d11
MadV313 <snowvonia@gmail.com>
2025-05-23 12:44:08 -0400

    Create channel_ultis.py

---
+++ utils/channel_ultis.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+CHANNELS_FILE = "data/channels.json"
+
+def load_channels():
+    if not os.path.exists(CHANNELS_FILE):
+        return {}
+    with open(CHANNELS_FILE, "r") as f:
+        return json.load(f)
+
+def save_channel(channel_type: str, channel_id: str):
+    data = load_channels()
+    data[channel_type] = channel_id
+    with open(CHANNELS_FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+def get_channel_id(channel_type: str) -> str | None:
+    data = load_channels()
+    return data.get(channel_type)
