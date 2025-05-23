18a906ec76b332bf032f634a5cf400fd39dcdc57
MadV313 <snowvonia@gmail.com>
2025-05-23 14:27:20 -0400

    Update channel_ultis.py

--- utils/channel_ultis.py
+++ utils/channel_ultis.py
@@ -10,6 +10,7 @@ def load_channels():
         return json.load(f)
 
 def save_channel(channel_type: str, channel_id: str):
+    os.makedirs(os.path.dirname(CHANNELS_FILE), exist_ok=True)
     data = load_channels()
     data[channel_type] = channel_id
     with open(CHANNELS_FILE, "w") as f:
